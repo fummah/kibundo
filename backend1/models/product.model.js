e62516f632dfa78c7407177be99dd4a704dcf0e0
fummah <fummah3@gmail.com>
2025-09-21 14:40:57 +0200

    msg

--- backend1/models/product.model.js
+++ backend1/models/product.model.js
@@ -1,18 +1,39 @@
 module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define('Product', {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    stripe_product_id: { type: DataTypes.TEXT, unique: true },
+    id: { 
+      type: DataTypes.INTEGER, 
+      autoIncrement: true, 
+      primaryKey: true 
+    },
+    stripe_product_id: { 
+      type: DataTypes.TEXT, 
+      unique: true 
+    },
     name: DataTypes.TEXT,
     description: DataTypes.TEXT,
-    price: {type: Sequelize.DECIMAL(10, 2),
-      allowNull: false,
-      defaultValue: 0},
-    active: { type: DataTypes.BOOLEAN, defaultValue: true },
-    metadata: { type: DataTypes.JSONB, defaultValue: {} },
-    created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-    updated_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    price: { 
+      type: DataTypes.DECIMAL(10, 2), 
+      allowNull: false, 
+      defaultValue: 0 
+    },
+    active: { 
+      type: DataTypes.BOOLEAN, 
+      defaultValue: true 
+    },
+    metadata: { 
+      type: DataTypes.JSONB, 
+      defaultValue: {} 
+    },
+    created_at: { 
+      type: DataTypes.DATE, 
+      defaultValue: DataTypes.NOW 
+    },
+    updated_at: { 
+      type: DataTypes.DATE, 
+      defaultValue: DataTypes.NOW 
+    },
     created_by: {
-      type: DataTypes.STRING, // updated from TIME to STRING (VARCHAR)
+      type: DataTypes.STRING, 
       allowNull: true
     }
   }, {
@@ -20,12 +41,12 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: false
   });
 
-    Product.associate = (models) => {
-        Product.hasMany(models.subscription, {
-    foreignKey: 'plan_id',
-    as: 'product'
-  });
+  Product.associate = (models) => {
+    Product.hasMany(models.subscription, {
+      foreignKey: 'plan_id',
+      as: 'product'
+    });
   };
+
   return Product;
 };
-
